4096396ebb764b631c111c64b500435589078b1a
Wender <wendermaiac@gmail.com>
2024-11-14 08:54:31 -0300

    Fix: Database Context database injection fixed on repositories

--- Program.cs
+++ Program.cs
@@ -117,7 +117,7 @@ app.UseSwagger();
 app.UseSwaggerUI(options =>
 {
     options.SwaggerEndpoint("/swagger/v1/swagger.json", "Smart Agro API v1");
-    //options.RoutePrefix = string.Empty;
+    options.RoutePrefix = string.Empty;
 });
 
 app.UseHttpsRedirection();
